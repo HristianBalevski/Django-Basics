41fca32b32dab657464578d24de036746f09ba2f
Hristian Balevski <hb.hris.bal@gmail.com>
2024-09-27 14:14:15 +0100

    Added new feature

--- petstagram/petstagram/photos/urls.py
+++ petstagram/petstagram/photos/urls.py
@@ -4,7 +4,7 @@ from petstagram.photos import views
 urlpatterns = (
     path('', views.add_photo, name='add-photo'),
     path('<int:pk>/', include([
-        path('', views.photo_details, name='photo-details'),
+        path('photo/', views.photo_details, name='photo-details'),
         path('edit/', views.edit_photo, name='photo-edit'),
     ]))
 )
