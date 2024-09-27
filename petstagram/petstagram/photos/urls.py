d4712494a06651a4ce3d840e94bd790ffc312732
Hristian Balevski <hb.hris.bal@gmail.com>
2024-09-27 09:57:57 +0100

    feat: added 4 new apps and their views and urls

--- petstagram/petstagram/photos/urls.py
+++ petstagram/petstagram/photos/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from petstagram.photos import views
 
 urlpatterns = (
-    path('photos/', views.add_photo, name='add-photo'),
-    path('photos/<int:pk>/', include([
+    path('', views.add_photo, name='add-photo'),
+    path('<int:pk>/', include([
         path('', views.photo_details, name='photo-details'),
         path('edit/', views.edit_photo, name='photo-edit'),
     ]))
