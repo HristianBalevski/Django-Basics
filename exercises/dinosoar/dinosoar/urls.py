e384f8f2ca08febd445a2cb6047296f54be20893
Hristian Balevski <hb.hris.bal@gmail.com>
2024-10-01 18:40:58 +0100

    Added new project

---
+++ exercises/dinosoar/dinosoar/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from dinofacts.views import show_dino
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("show_dino/<str:name>/", show_dino),
+]
